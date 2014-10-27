853319b7d94af59be3f2a71b16b3eb591abcdae4
Rukshan Ranatunge <rukshan@outlook.com>
2014-10-27 20:52:35 +0530

    Create config.php

---
+++ api/config.php
@@ -0,0 +1,8 @@
+<?php
+/**
+* @file
+* A single location to store configuration.
+*/
+define('CONSUMER_KEY', 'TWITTER CONSUMER KEY');
+define('CONSUMER_SECRET', 'TWITTER_CONSUMER_SECRET');
+define('OAUTH_CALLBACK', 'TWITTER APP CALLBACK URL');
